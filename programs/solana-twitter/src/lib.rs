7189b667d21c1370a94dd10d4777faf6765647c6
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-04-27 15:41:30 +0500

    structuring-our-tweet-account

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -13,3 +13,26 @@ pub mod solana_twitter {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Tweet {
+    pub auther: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_TOPIC_LENGTH: usize = 50 * 4;
+const MAX_CONTENT_LENGTH: usize = 280 * 4;
+
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+    + PUBLIC_KEY_LENGTH // Author
+    + TIMESTAMP_LENGTH // Timestamp
+    + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic
+    + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // content
+}
