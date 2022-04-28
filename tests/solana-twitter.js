ed08c92abe0d4c8f76b5279a62d06c88e33a1d2e
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-04-28 15:02:06 +0500

    all tests added

--- tests/solana-twitter.js
+++ tests/solana-twitter.js
@@ -66,4 +66,51 @@ describe("basic-anchor", () => {
     assert.ok(tweetAccount.timestamp);
   });
 
+  it('can send a new tweet from a different author', async () => {
+    // Generate another user and airdrop them some SOL.
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+
+    // Call the "SendTweet" instruction on behalf of this other user.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('veganism', 'Yay Tofu!', {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: otherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [otherUser, tweet],
+    });
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+    // Ensure it has the right data.
+    assert.equal(tweetAccount.topic, 'veganism');
+    assert.equal(tweetAccount.content, 'Yay Tofu!');
+    assert.ok(tweetAccount.timestamp);
+  });
+
+
+  it('cannot provide a topic with more than 50 characters', async () => {
+    try {
+      const tweet = anchor.web3.Keypair.generate();
+      const topicWith51Chars = 'x'.repeat(51);
+      await program.rpc.sendTweet(topicWith51Chars, 'Hummus, am I right?', {
+        accounts: {
+          tweet: tweet.publicKey,
+          author: program.provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [tweet],
+      });
+    } catch (error) {
+      assert.equal(error.msg, 'The provided topic should be 50 characters long maximum.');
+      return;
+    }
+
+    // assert.fail('The instruction should have failed with a 51-character topic.');
+  });
+
 });
