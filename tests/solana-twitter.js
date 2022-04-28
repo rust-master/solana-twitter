42aa00f63ae3b5d4b65a1e182623ede8bdd94ad6
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-04-28 14:51:58 +0500

    mocha test

--- tests/solana-twitter.js
+++ tests/solana-twitter.js
@@ -11,21 +11,20 @@ describe("basic-anchor", () => {
 
   let _myAccount = null;
 
+  const idl = JSON.parse(
+    require("fs").readFileSync("./target/idl/solana_twitter.json", "utf8")
+  );
+
+  //Address of the deployed program
+  const programId = new anchor.web3.PublicKey(
+    "98d32F5rG4bePA4Wx7iEqMy9iEeTF1T78avNqA4Zydku"
+  );
+
+  //Generate the program client from IDL
+  const program = new anchor.Program(idl, programId);
+
+
   it("send tweet", async () => {
-    // #region code-simplified
-    // The program to execute.
-    const idl = JSON.parse(
-      require("fs").readFileSync("./target/idl/solana_twitter.json", "utf8")
-    );
-
-    //Address of the deployed program
-    const programId = new anchor.web3.PublicKey(
-      "98d32F5rG4bePA4Wx7iEqMy9iEeTF1T78avNqA4Zydku"
-    );
-
-    //Generate the program client from IDL
-    const program = new anchor.Program(idl, programId);
-
     const tweet = anchor.web3.Keypair.generate();
     await program.rpc.sendTweet("pakistan", "I love paksitan", {
       accounts: {
@@ -44,4 +43,27 @@ describe("basic-anchor", () => {
 
     _myAccount = tweet;
   });
+
+  it('can send a new tweet without a topic', async () => {
+    // Call the "SendTweet" instruction.
+    const tweet = anchor.web3.Keypair.generate();
+    await program.rpc.sendTweet('', 'gm', {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweet],
+    });
+
+    // Fetch the account details of the created tweet.
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+
+    // Ensure it has the right data.
+    // assert.equal(tweetAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, '');
+    assert.equal(tweetAccount.content, 'gm');
+    assert.ok(tweetAccount.timestamp);
+  });
+
 });
