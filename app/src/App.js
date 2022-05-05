5beb5e0be18ef2c2a594fdc1cf7ecd2e8aa6a5b0
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-05-05 11:11:21 +0500

    Sidebar added

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SideNavbar from './components/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div>
+      <div className="app">
+        <SideNavbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
